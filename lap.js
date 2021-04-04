8d3cf934f2bbf8fed2c33a33175e3a09f56740ee
shahd <shahd.jm33@gmail.com>
2021-04-04 22:24:40 +0300

    ask the user how many image

--- lap.js
+++ lap.js
@@ -13,21 +13,16 @@ while(clothesType !== 'shirts' && clothesType !== 'pants')
      clothesType = prompt('please choose shirts or pants');
 }
 
-if(clothesType == "shirts")
-{
-    document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.PpE3AdumDDkCSMoy1gG5pAHaHa?w=206&h=206&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
 
-    document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.yY8ZWeFHLmdz_EGN2ZBhjwHaHa?w=213&h=213&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
+var number = prompt('how many do you want');
 
-    document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.-goKNgDU0a_adN-EHzNzhQHaHa?w=198&h=194&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
-
-}
-if(clothesType == "pants")
-{
-    document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.Zc-DGdq4Q1nT1dum9TpnTgHaJQ?w=182&h=228&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
-
-    document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.PgsGDOykReMhXelpxgXkPgHaIq?w=182&h=213&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
-
-    document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.YXnYfTMpPZ4gyQCBAPJfAAHaJd?w=182&h=232&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
-
+for (var index = 0; index < number; index++) {
+    if(clothesType == "shirts")
+    {
+        document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.PpE3AdumDDkCSMoy1gG5pAHaHa?w=206&h=206&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
+    }
+    if(clothesType == "pants")
+    {
+        document.write('<img style="width: 19%;" src="https://th.bing.com/th/id/OIP.Zc-DGdq4Q1nT1dum9TpnTgHaJQ?w=182&h=228&c=7&o=5&dpr=1.25&pid=1.7" alt=""/>');
+    }
 }
